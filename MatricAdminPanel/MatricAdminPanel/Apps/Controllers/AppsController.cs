6b9907c94537cab7f454b4473939a10f8dfe8fb9
farajov ewqin <eshqin115@gmail.com>
2022-11-13 02:25:57 +0400

    Create chat,calendar contact-list pages

---
+++ MatricAdminPanel/MatricAdminPanel/Apps/Controllers/AppsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MatricAdminPanel.Apps.Controllers
+{
+    public class AppsController : Controller
+    {
+        public ActionResult Chat()
+        {
+            return View();
+        }
+        public ActionResult ContactList()
+        {
+            return View();
+        }
+        public ActionResult Calendar()
+        {
+            return View();
+        }
+        public ActionResult Invoice()
+        {
+            return View();
+        }
+    }
+}
