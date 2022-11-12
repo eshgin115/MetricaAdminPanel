2f6441284a8a4fc5f793d5fc413d473c71b68514
farajov ewqin <eshqin115@gmail.com>
2022-11-12 22:45:54 +0400

    Create Folder Structur

---
+++ MatricAdminPanel/MatricAdminPanel/Controllers/DashBoard/AnalyticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MatricAdminPanel.Controllers.DashBoard
+{
+    public class AnalyticsController :Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MatricAdminPanel/MatricAdminPanel/Controllers/DashBoard/CryptoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MatricAdminPanel.Controllers.DashBoard
+{
+    public class CryptoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
